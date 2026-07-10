2849f636509b5a906b7a58388a7828121cbb486c
MUHAMMED AJMAL P <muhammedajmalp37@gmail.com>
2026-07-10 12:24:35 +0530

    ✅ Invert Binary Tree | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 19.4 MB | 2026-07-10

---
+++ leetcode/easy/0226-invert-binary-tree/solution.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def swap(node):
+            if node:
+                swap(node.left)
+                swap(node.right)
+                node.left,node.right=node.right,node.left
+        swap(root)
+        return(root)
